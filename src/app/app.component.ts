8b69f755fe2a0369c5e8d260d6810e00de6534c8
CarlaLDC <carla9lima0carvalho@gmail.com>
2026-04-25 14:29:08 -0300

    criação da tela inicial (html e css)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent,
+    HomeComponent,
+    FilterComponent,
+    FooterComponent,
+    ProdutosComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: '', component: HeaderComponent},
+    { path: 'filter', component: FilterComponent },
+    { path: 'produtos', component: ProdutosComponent },
+    { path: '', component: FooterComponent}
 ];
  
