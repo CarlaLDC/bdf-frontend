7ef74e859887b1d02dbd8cf784296ff289053386
Camila-cvs <camila.souzacvs@gmail.com>
2026-04-30 21:09:30 -0300

    separando os filtros

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProdutoServices } from '../../services/produto/produto.component';
+import { CommonModule } from '@angular/common';
+ 
 @Component({
   selector: 'app-filter',
   imports: [],
@@ -7,5 +9,59 @@ import { Component } from '@angular/core';
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+    produtos: any[] = [];
+    loading = false;
+   
+    // Filtros
+    publicoSelecionado: string | null = null;
+    tipoSelecionado: string | null = null;
+    nomeBusca: string = '';
+   
+    constructor(private ProdutoServices: ProdutoServices) {}
+   
+    ngOnInit() {
+      this.carregarProdutos();
+    }
+   
+    carregarProdutos() {
+      this.loading = true;
+      
+      this.ProdutoServices.getProducts(
+        this.publicoSelecionado || undefined,
+        this.tipoSelecionado || undefined,
+        this.nomeBusca || undefined
+      ).subscribe({
+        next: (response) => {
+          this.produtos = response.data;
+          this.loading = false;
+        },
+        error: (error) => {
+          console.error('Erro ao carregar produtos:', error);
+          this.loading = false;
+        }
+      });
+    }
+   
+    filtrarPorPublico(publico: string | null) {
+      this.publicoSelecionado = publico;
+      this.carregarProdutos();
+    }
+   
+    filtrarPorTipo(tipo: string | null) {
+      this.tipoSelecionado = tipo;
+      this.carregarProdutos();
+    }
+   
+    buscar(nome: string) {
+      this.nomeBusca = nome;
+      this.carregarProdutos();
+    }
+   
+    limparFiltros() {
+      this.publicoSelecionado = null;
+      this.tipoSelecionado = null;
+      this.nomeBusca = '';
+      this.carregarProdutos();
+    }
 
 }
