c14118794070b4e43df40f569209248cab88d7bd
tiphy <souzatiphany55@gmail.com>
2026-05-06 22:10:24 -0300

    criação do menu carrinho

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuCarrinhoAberto = false;
 
+  menuCarrinho() {
+    this.menuCarrinhoAberto = !this.menuCarrinhoAberto;
+
+  }
 }
