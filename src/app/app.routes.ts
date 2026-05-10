e879d9f4b6ec37bb4aafc057f1520d271712e27b
CarolinaMagalhaesLopes <carolinamagalhaeslopes9@gmail.com>
2026-05-09 21:37:50 -0300

    Atualização: arrumei a rota do ver detalhe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AdicionarProdutoComponent } from './components/adicionar-produto/adicionar-produto.component';
 import { GerenciarProdutoComponent } from './components/gerenciar-produto/gerenciar-produto.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { ProdutoDetalheComponent } from './components/detalhe-produto/detalhe-produto.component';
 import { SobreNos } from './components/sobre-nos/sobre-nos';
 import { PerfilCliente } from './components/perfil-cliente/perfil-cliente';
 import { PerfilAdmin } from './components/perfil-admin/perfil-admin.component';
@@ -11,6 +13,8 @@ export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'adicionar-produto', component: AdicionarProdutoComponent },
   { path: 'gerenciar-produto', component: GerenciarProdutoComponent },
+    { path: 'produtos', component: ProdutosComponent },
+    { path: 'produto/:id', component: ProdutoDetalheComponent },
   { path: 'sobre-nos', component: SobreNos },
   { path: 'perfil-cliente', component: PerfilCliente },
   { path: 'perfil-admin', component: PerfilAdmin },
